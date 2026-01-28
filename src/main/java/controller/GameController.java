42fd1366b83a39f17a40b21ff1ad0cbad3fdde8d
sapiensxxv <jhspacelover@naver.com>
2026-01-28 18:07:20 +0900

    feat: 게임 컨트롤러

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -1,8 +1,50 @@
-/**
- * 애플리케이션 진입점
- */
+package controller;
+
+import dto.GameResult;
+import model.BaseballNumber;
+import model.InputConverter;
+import model.RandomNumberGenerator;
+import model.Referee;
+import view.InputView;
+import view.OutputView;
+
+
 public class GameController {
-    public static void main(String[] args) {
-        System.out.println("asdfasdfasdfsdf");
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final RandomNumberGenerator randomNumberGenerator;
+    private final Referee referee;
+
+    public GameController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        this.randomNumberGenerator = new RandomNumberGenerator();
+        this.referee = new Referee();
     }
+
+    public void run() {
+        BaseballNumber computerNumber = randomNumberGenerator.generate();
+
+        System.out.println("컴퓨터 숫자 생성 완료 (테스트용): " + computerNumber.getNumbers());
+    }
+
+    private void play(BaseballNumber computerNumber) {
+        while(true) {
+            try {
+                String inputString = inputView.inputNumbers();
+                BaseballNumber userNumber = new BaseballNumber(InputConverter.convertStringToIntegerList(inputString));
+                GameResult result = referee.judge(computerNumber, userNumber);
+                outputView.printResult(result);
+
+                if (result.isThreeStrike()) {
+                    outputView.printGameSuccess();
+                    break;
+                }
+            } catch (IllegalArgumentException e) {
+                outputView.printError(e.getMessage());
+            }
+        }
+    }
+
 }
