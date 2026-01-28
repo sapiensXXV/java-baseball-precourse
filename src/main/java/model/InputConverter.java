ae602f6cda584e5532802a9030e33b0e5ed03bb8
sapiensxxv <jhspacelover@naver.com>
2026-01-28 18:03:15 +0900

    feat: 사용자 입력

--- src/main/java/model/InputConverter.java
+++ src/main/java/model/InputConverter.java
@@ -1,4 +1,27 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputConverter {
+
+    private InputConverter() {}
+
+    public static List<Integer> convertStringToIntegerList(String input) {
+        // 사용자의 공백 입력 실수 방지
+        String trimmedInput = input.trim();
+
+        List<Integer> numbers = new ArrayList<>();
+
+        for (char character : trimmedInput.toCharArray()) {
+            if (!Character.isDigit(character)) {
+                throw new IllegalArgumentException("[ERROR] 숫자만 입력해주세요");
+            }
+
+            int number = Character.getNumericValue(character);
+            numbers.add(number);
+        }
+
+        return numbers;
+    }
 }
