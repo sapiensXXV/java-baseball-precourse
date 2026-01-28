09561bec6f1f89ae3be1c42e8d7701ddafb0eb3d
sapiensxxv <jhspacelover@naver.com>
2026-01-28 18:06:50 +0900

    feat: 사용자 입력 클래스

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,4 +1,20 @@
 package view;
 
-public class Inputview {
+import java.util.Scanner;
+
+public class InputView {
+    private static final String INPUT_MESSAGE = "숫자를 입력해 주세요 : ";
+    private final Scanner scanner;
+
+    public InputView() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String inputNumbers() {
+        System.out.println(INPUT_MESSAGE);
+
+        String input = scanner.nextLine();
+
+        return input.trim();
+    }
 }
