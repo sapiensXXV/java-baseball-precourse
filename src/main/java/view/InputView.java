80b587758ad67b888fe3c3ff090c74265538001b
sapiensxxv <jhspacelover@naver.com>
2026-01-28 18:12:14 +0900

    feat: 재시도 메세지 출력

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class InputView {
     private static final String INPUT_MESSAGE = "숫자를 입력해 주세요 : ";
+    private static final String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private final Scanner scanner;
 
     public InputView() {
@@ -16,5 +17,10 @@ public class InputView {
         String input = scanner.nextLine();
 
         return input.trim();
+    }
+
+    public String inputRestartCommand() {
+        System.out.println(RESTART_MESSAGE); // 안내 문구 출력
+        return scanner.nextLine().trim();    // 입력받은 값(1 또는 2) 반환
     }
 }
