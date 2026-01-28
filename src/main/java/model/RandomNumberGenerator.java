6079eb449b19c1d642616e34e5acfed69a089c43
sapiensxxv <jhspacelover@naver.com>
2026-01-28 18:06:20 +0900

    chore: 생성자 누락 수정

--- src/main/java/model/RandomNumberGenerator.java
+++ src/main/java/model/RandomNumberGenerator.java
@@ -4,16 +4,10 @@ import java.util.*;
 
 public class RandomNumberGenerator {
 
-    private static RandomNumberGenerator instance = new RandomNumberGenerator();
-    private static Random random = new Random();
-    private HashSet<Integer> numbers = new HashSet<>(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9));
+    private final Random random;
 
-
-    private RandomNumberGenerator() {
-    }
-
-    public static RandomNumberGenerator getInstance() {
-        return instance;
+    public RandomNumberGenerator() {
+        this.random = new Random();
     }
 
     public BaseballNumber generate() {
