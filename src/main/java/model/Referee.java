7aa1606682aaa6edad47ac0b4a4a4cf98013a208
sapiensxxv <jhspacelover@naver.com>
2026-01-28 17:40:11 +0900

    feat: 게임 판정 로직

--- src/main/java/model/Referee.java
+++ src/main/java/model/Referee.java
@@ -1,4 +1,26 @@
 package model;
 
+import dto.GameResult;
+
 public class Referee {
+
+    public GameResult judge(BaseballNumber computer, BaseballNumber user) {
+        int balls = 0;
+        int strikes = 0;
+
+        for (int i = 0; i < Constant.NUMBERS_SIZE; i++) {
+            int userNumber = user.getNumber(i);
+
+            // 스트라이크인지 확인 (위치와 숫자가 모두 같음);
+            if (computer.getNumber(i) == userNumber) {
+                strikes++;
+                continue; // 볼 확인은 건너뜀
+            }
+            if (computer.contain(userNumber)) {
+                balls++;
+            }
+        }
+
+        return new GameResult(balls, strikes);
+    }
 }
