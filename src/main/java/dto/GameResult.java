ec9efce9ae3794fa078c7c1345d3793f81ec7d2b
sapiensxxv <jhspacelover@naver.com>
2026-01-28 18:02:28 +0900

    add: 게임 결과 DTO

--- src/main/java/dto/GameResult.java
+++ src/main/java/dto/GameResult.java
@@ -1,4 +1,27 @@
 package dto;
 
 public class GameResult {
+    private final int ball;
+    private final int strike;
+
+    public GameResult(int ball, int strike) {
+        this.ball = ball;
+        this.strike = strike;
+    }
+
+    public boolean isNothing() {
+        return ball == 0 && strike == 0;
+    }
+
+    public boolean isThreeStrike() {
+        return strike == 3;
+    }
+
+    public int getBall() {
+        return ball;
+    }
+
+    public int getStrike() {
+        return strike;
+    }
 }
