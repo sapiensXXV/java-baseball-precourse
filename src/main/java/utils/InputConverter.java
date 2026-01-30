dde095cab4472b3241d6026854931bbced927859
sapiensxxv <jhspacelover@naver.com>
2026-01-30 18:39:07 +0900

    feat: 숫자 입력 검증로직

--- src/main/java/utils/InputConverter.java
+++ src/main/java/utils/InputConverter.java
@@ -10,20 +10,43 @@ public class InputConverter {
     private InputConverter() {}
 
     public static List<Integer> convertStringToIntegerList(String input) {
-        // 사용자의 공백 입력 실수 방지
-        String trimmedInput = input.trim();
-
-        List<Integer> numbers = new ArrayList<>();
-
-        for (char character : trimmedInput.toCharArray()) {
-            if (!Character.isDigit(character)) {
-                throw new IllegalArgumentException(NOT_NUMBER);
-            }
-
-            int number = Character.getNumericValue(character);
-            numbers.add(number);
+        // null 및 공백처리
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException(NOT_NUMBER);
         }
 
+        String trimmedInput = input.trim();
+
+        // 길이검증
+        validateLength(trimmedInput);
+        List<Integer> numbers = new ArrayList<>();
+
+        for (char character: trimmedInput.toCharArray()) {
+            validateDigit(character);
+            int number = Character.getNumericValue(character);
+            validateRange(number);
+            numbers.add(number);
+        }
         return numbers;
     }
+
+    private static void validateLength(String input) {
+        if (input.length() != 3) {
+            throw new IllegalArgumentException(OUT_OF_RANGE);
+        }
+    }
+
+    private static void validateDigit(char number) {
+        if (!Character.isDigit(number)) {
+            throw new IllegalArgumentException(OUT_OF_RANGE);
+        }
+    }
+
+    private static void validateRange(int number) {
+        if (number == 0) {
+            throw new IllegalArgumentException(OUT_OF_RANGE);
+        }
+    }
+
+
 }
